a7b83fa8bf641837a4cc634ab59a25dcff22ed81
IvanMasnari <i.masnari@exin.digital>
2023-05-13 13:53:29 +0200

    removed secrets

--- upload_docs.py
+++ upload_docs.py
@@ -14,43 +14,56 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 PINECONE_API_ENV = os.environ.get('PINECONE_API_ENV')
 
-loader = PyPDFLoader('docs/SenecaLettereLucilio.pdf')
-
-data = loader.load()
-
-print (f'You have {len(data)} document(s) in your data')
-print (f'There are {len(data[30].page_content)} characters in your document')
-
-splitter = RecursiveCharacterTextSplitter(
-    # Set a really small chunk size, just to show.
-    chunk_size = 1000,
-    chunk_overlap  = 20,
-)
-
-chunks = splitter.split_documents(data)
-
-print (f'Now you have {len(chunks)} documents')
-
 embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
 
+
+
 pinecone.init(
-    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
-    environment=PINECONE_API_ENV  # next to api key in console
-)
+        api_key=PINECONE_API_KEY,  # find at app.pinecone.io
+        environment=PINECONE_API_ENV  # next to api key in console
+    )
 index_name = "langchaintest"
 
-pinecone.delete_index(index_name)
+splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 1000,
+        chunk_overlap  = 20,
+)
 
-if index_name not in pinecone.list_indexes():
+print(OPENAI_API_KEY)
+print(PINECONE_API_KEY)
+print(PINECONE_API_ENV)
 
-    pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
-    docsearch = Pinecone.from_texts([t.page_content for t in chunks], embeddings, index_name=index_name)
+def load_pdf(path):
 
-else:
-    
-    docsearch = Pinecone.from_existing_index(index_name, embeddings)
+    loader = PyPDFLoader(path)
 
-query = "Quali sono le caratteristiche di un buon amico?"
-docs = docsearch.similarity_search(query)
+    data = loader.load()
 
-print(docs)
+    print (f'You have {len(data)} document(s) in your data')
+    print (f'There are {len(data[0].page_content)} characters in your document')
+
+    chunks = splitter.split_documents(data)
+
+    print (f'Now you have {len(chunks)} documents')
+
+    if index_name not in pinecone.list_indexes():
+
+        pinecone.create_index(name=index_name, metric="cosine", dimension=1536)
+        index = pinecone.Index(index_name=index_name)  
+        vectorstore = Pinecone(index, embeddings.embed_query, "text")
+        vectorstore.from_texts([t.page_content for t in chunks], embeddings, index_name=index_name)
+
+    else:
+
+        index = pinecone.Index(index_name=index_name)  
+        vectorstore = Pinecone(index, embeddings.embed_query, "text")
+        vectorstore.add_texts([t.page_content for t in chunks])
+
+
+if __name__ == "__main__":
+
+    for file in os.listdir('docs'):
+
+        if file.endswith('.pdf'):
+
+            load_pdf(f'docs/{file}')
